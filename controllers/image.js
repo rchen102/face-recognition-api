ddc2949a82251c33dbdc0a453b5e32f653902c28
rchen102 <rchen102@binghamton.edu>
2018-08-15 21:23:13 -0400

    back-end

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'a25dbb149469440084aaaf52341ab7ed'
+});
+
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict("a403429f2ddf4b49b307e318f00e528b", req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id','=',id)
+	.increment('entries',1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+	handleImage,
+	handleApiCall
+};
