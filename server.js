75d882029e360e41dea547b43a3068381c1a6819
rchen102 <rchen102@binghamton.edu>
2018-09-02 19:24:38 -0400

    change database url

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.json(database.users) })
+app.get('/', (req, res) => { res.json('it is working') })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
